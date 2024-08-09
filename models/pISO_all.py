0f14a82714c363688f840fde3d3e80d7e4fcd41d
stavros-vlach <122750141+stavros-vlach@users.noreply.github.com>
2024-08-09 19:40:50 +0300

    Update pISO_all.py

--- models/pISO_all.py
+++ models/pISO_all.py
@@ -33,11 +33,68 @@ labels = ["Galaxy",
 
 #constants
 G = 4.3e-6
+Y_hat_star = 0.5
+sigma_Y_star = 0.25 * Y_hat_star
+#Model as in https://iopscience.iop.org/article/10.3847/2041-8213/ac1bb7
 
-path = "/Rotmod_LTG/"
+def pISO_halo(logrho0, logRc):
+	rho0 = 10 ** logrho0
+	Rc = 10 ** logRc
+	v_halo_sq = 4 * np.pi * G * rho0 * Rc ** 2 * ( 1 - (Rc / r) * np.arctan(r / Rc))
+	return v_halo_sq
+	
+def total_velocity_squared(Y_star, logrho0, logRc):
+	rho0 = 10 ** logrho0
+	Rc = 10 ** logRc
+	v_star_sq = Y_star * v_disk_sq + 1.4 * Y_star * v_bulge_sq
+	v_halo_sq = pISO_halo(logrho0, logRc)
+	v_total_sq = v_star_sq + v_gas_sq + v_halo_sq
+	return v_total_sq
+	
+def log_prior(Y_star, logrho0, logRs):
+	"""Define the log prior."""
+	rho0 = 10 ** logrho0
+	Rs = 10 ** logRs
+	if 0 < Y_star < 1 and 0 < rho0 < 1e9 and 0 < Rs < 1e6:
+	    return 0.0
+	return -np.inf
 
+def log_likelihood(theta):
+	"""Define the log likelihood."""
+	Y_star, logrho0, logRc = theta
+	v_model_sq = total_velocity_squared(Y_star, logrho0, logRc)
+	chi_sq = np.sum(((v_obs**2 - v_model_sq) / v_err**2) ** 2)
+	chi_sq += ((Y_star - Y_hat_star) / sigma_Y_star) ** 2
+	return -0.5 * chi_sq
+
+def log_probability(theta):
+	"""Define the log probability function for emcee."""
+	Y_star, logrho0, logRc = theta
+	lp = log_prior(Y_star, logrho0, logRc)
+	if not np.isfinite(lp):
+	    return -np.inf
+	return lp + log_likelihood(theta)
+
+
+def chi_square(theta):
+	"""Calculate the chi-square value for the model fit."""
+	Y_star, logrho0, logRc = theta
+	v_model_sq = total_velocity_squared(Y_star, logrho0, logRc)
+	chi_sq = np.sum(((v_obs**2 - v_model_sq) / v_err**2) ** 2)
+	chi_sq += ((Y_star - Y_hat_star) / sigma_Y_star) ** 2
+	return chi_sq / (len(r) - len(theta))  # Adjusted for degrees of freedom
+
+#bounds for differential_evolution
+bounds = [(0, 1), (0, 10), (0, 6)]
+
+#parameters for emcee
+ndim = 3
+nwalkers = 100
+nsteps = 1000
+
+path = "/media/stavros/aaf194dd-624d-4ece-8e56-395e27a93185/Pythonproject/Rotmod_LTG/"
 for GALAXY_NAME in os.listdir(path):
-	name = "/Rotmod_LTG/" + GALAXY_NAME
+	name = path + GALAXY_NAME
 	R, V, Verr, Vgas, Vbul, Vdisk = np.loadtxt(name, unpack=True, usecols=(0, 1, 2, 3, 4, 5))
 	data_length = len(R)
 	
@@ -58,56 +115,6 @@ for GALAXY_NAME in os.listdir(path):
 	Lb = Lb[Lb['Galaxy'] == GALAXY_NAME]
 	L_bul = Lb['Lbul (10^9 Lsun)'].to_numpy()[0]
 
-
-	#Model as in https://iopscience.iop.org/article/10.3847/2041-8213/ac1bb7
-
-	def pISO_halo(logrho0, logRc):
-		rho0 = 10 ** logrho0
-		Rc = 10 ** logRc
-		v_halo_sq = 4 * np.pi * G * rho0 * Rc ** 2 * ( 1 - (Rc / r) * np.arctan(r / Rc))
-		return v_halo_sq
-		
-	def total_velocity_squared(Y_star, logrho0, logRc):
-		rho0 = 10 ** logrho0
-		Rc = 10 ** logRc
-		v_star_sq = Y_star * v_disk_sq + 1.4 * Y_star * v_bulge_sq
-		v_halo_sq = pISO_halo(logrho0, logRc)
-		v_total_sq = v_star_sq + v_gas_sq + v_halo_sq
-		return v_total_sq
-		
-	def log_prior(Y_star, logrho0, logRs):
-		"""Define the log prior."""
-		rho0 = 10 ** logrho0
-		Rs = 10 ** logRs
-		if 0 < Y_star < 1 and 0 < rho0 < 1e9 and 0 < Rs < 1e6:
-		    return 0.0
-		return -np.inf
-
-	def log_likelihood(theta):
-		"""Define the log likelihood."""
-		Y_star, logrho0, logRc = theta
-		v_model_sq = total_velocity_squared(Y_star, logrho0, logRc)
-		chi_sq = np.sum(((v_obs**2 - v_model_sq) / v_err**2) ** 2)
-		chi_sq += ((Y_star - Y_hat_star) / sigma_Y_star) ** 2
-		return -0.5 * chi_sq
-
-	def log_probability(theta):
-		"""Define the log probability function for emcee."""
-		Y_star, logrho0, logRc = theta
-		lp = log_prior(Y_star, logrho0, logRc)
-		if not np.isfinite(lp):
-		    return -np.inf
-		return lp + log_likelihood(theta)
-
-
-	def chi_square(theta):
-		"""Calculate the chi-square value for the model fit."""
-		Y_star, logrho0, logRc = theta
-		v_model_sq = total_velocity_squared(Y_star, logrho0, logRc)
-		chi_sq = np.sum(((v_obs**2 - v_model_sq) / v_err**2) ** 2)
-		chi_sq += ((Y_star - Y_hat_star) / sigma_Y_star) ** 2
-		return chi_sq / (len(r) - len(theta))  # Adjusted for degrees of freedom
-		
 	# Observed data and errors
 	r = np.array(R)
 	v_obs = np.array(V)
@@ -115,10 +122,6 @@ for GALAXY_NAME in os.listdir(path):
 	v_disk_sq = np.array(Vdisk)**2
 	v_bulge_sq = np.array(Vbul)**2
 	v_gas_sq = np.array(Vgas)**2
-	Y_hat_star = 0.5
-	sigma_Y_star = 0.25 * Y_hat_star
-
-	bounds = [(0, 1), (0, 10), (0, 6)]
 
 	result = differential_evolution(chi_square, bounds)
 	best_fit_params = result.x
@@ -133,47 +136,44 @@ for GALAXY_NAME in os.listdir(path):
 	plt.ylabel('$V(km/s)$')
 	plt.xlabel('$R(kpc)$')
 	plt.legend()
-	plt.savefig(GALAXY_NAME + ' fit_results-beforeMCMC.pdf')
+	plt.savefig("/media/stavros/aaf194dd-624d-4ece-8e56-395e27a93185/Pythonproject/de/" + GALAXY_NAME + ' fit_results-beforeMCMC.pdf')
 	plt.close()
 
-ndim = len(best_fit_params)
-nwalkers = 100
-nsteps = 1000
+	# Set up the sampler
+	sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability)
 
-# Set up the sampler
-sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability)
+	# Initialize the walkers
+	pos = best_fit_params + 1e-2 * np.random.randn(nwalkers, ndim)
 
-# Initialize the walkers
-pos = best_fit_params + 1e-2 * np.random.randn(nwalkers, ndim)
+	# Run the sampler
+	sampler.run_mcmc(pos, nsteps, progress=True)
 
-# Run the sampler
-sampler.run_mcmc(pos, nsteps, progress=True)
+	# Extract the samples
+	samples = sampler.get_chain(discard=100, thin=10, flat=True)
 
-# Extract the samples
-samples = sampler.get_chain(discard=100, thin=10, flat=True)
+	fig = corner.corner(samples, labels=["$Y_\\star$", "$\\rho_0$", "$R_s$"], truths=[Y_hat_star, 0, 0])
+	fig.savefig("/media/stavros/aaf194dd-624d-4ece-8e56-395e27a93185/Pythonproject/de/" + GALAXY_NAME + "fit_corner.pdf")
+	plt.close()
 
-fig = corner.corner(samples, labels=["$Y_\\star$", "$\\rho_0$", "$R_s$"], truths=[Y_hat_star, 0, 0])
-fig.savefig(GALAXY_NAME + "fit_corner.pdf")
-plt.close()
+	Y_star_mcmc, logrho0_mcmc, logRs_mcmc = np.percentile(samples, 50, axis=0)
+	Y_star_mcmc_err = np.percentile(samples[:, 0], [16, 84])
+	logrho0_mcmc_err = np.percentile(samples[:, 1], [16, 84])
+	logRs_mcmc_err = np.percentile(samples[:, 2], [16, 84])
 
-Y_star_mcmc, logrho0_mcmc, logRs_mcmc = np.percentile(samples, 50, axis=0)
-Y_star_mcmc_err = np.percentile(samples[:, 0], [16, 84])
-logrho0_mcmc_err = np.percentile(samples[:, 1], [16, 84])
-logRs_mcmc_err = np.percentile(samples[:, 2], [16, 84])
+	print("Best-fit parameters and their 1-sigma intervals from MCMC:")
+	print(f"Y_star: {Y_star_mcmc:.4f} (+{Y_star_mcmc_err[1]-Y_star_mcmc:.4f}, -{Y_star_mcmc-Y_star_mcmc_err[0]:.4f})")
+	print(f"rho0: {10**logrho0_mcmc:.4e} (+{10**logrho0_mcmc_err[1]-10**logrho0_mcmc:.4e}, -{10**logrho0_mcmc-10**logrho0_mcmc_err[0]:.4e})")
+	print(f"Rs: {10**logRs_mcmc:.4f} (+{10**logRs_mcmc_err[1]-10**logRs_mcmc:.4f}, -{10**logRs_mcmc-10**logRs_mcmc_err[0]:.4f})")
 
-print("Best-fit parameters and their 1-sigma intervals from MCMC:")
-print(f"Y_star: {Y_star_mcmc:.4f} (+{Y_star_mcmc_err[1]-Y_star_mcmc:.4f}, -{Y_star_mcmc-Y_star_mcmc_err[0]:.4f})")
-print(f"rho0: {10**logrho0_mcmc:.4e} (+{10**logrho0_mcmc_err[1]-10**logrho0_mcmc:.4e}, -{10**logrho0_mcmc-10**logrho0_mcmc_err[0]:.4e})")
-print(f"Rs: {10**logRs_mcmc:.4f} (+{10**logRs_mcmc_err[1]-10**logRs_mcmc:.4f}, -{10**logRs_mcmc-10**logRs_mcmc_err[0]:.4f})")
+	best_fit_params = np.median(samples, axis=0)
+	Y_star_best, logrho0_best, logRs_best = best_fit_params
+	v_model_sq_mcmc = total_velocity_squared(Y_star_best, logrho0_best, logRs_best)
+	v_model_mcmc = np.sqrt(v_model_sq_best)
 
-best_fit_params = np.median(samples, axis=0)
-Y_star_best, logrho0_best, logRs_best = best_fit_params
-v_model_sq_mcmc = total_velocity_squared(Y_star_best, logrho0_best, logRs_best)
-v_model_mcmc = np.sqrt(v_model_sq_best)
-
-plt.errorbar(r, v_obs, yerr=v_err, fmt='o', label='Observed data')
-plt.plot(r, v_model_mcmc, label='Best-fit model after emcee', color='red')
-plt.xlabel('Radius (kpc)')
-plt.ylabel('Velocity (km/s)')
-plt.legend()
-plt.savefig(GALAXY_NAME + ' fit_results-afterMCMC.pdf')
+	plt.errorbar(r, v_obs, yerr=v_err, fmt='o', label='Observed data')
+	plt.plot(r, v_model_mcmc, label='Best-fit model after emcee', color='red')
+	plt.xlabel('Radius (kpc)')
+	plt.ylabel('Velocity (km/s)')
+	plt.legend()
+	plt.savefig("/media/stavros/aaf194dd-624d-4ece-8e56-395e27a93185/Pythonproject/de/" + GALAXY_NAME + ' fit_results-afterMCMC.pdf')
+	plt.close()
