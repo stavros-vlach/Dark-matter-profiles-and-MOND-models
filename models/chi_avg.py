e7ecc6d768b54cac9f8f5b295367d8decab36784
stavros-vlach <122750141+stavros-vlach@users.noreply.github.com>
2024-09-24 11:52:58 +0300

    Update chi_avg.py

--- models/chi_avg.py
+++ models/chi_avg.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv('/home/stavros/Documents/Pythonproject/parameters_pISO.csv')
+df = pd.read_csv('parameters_pISO.csv')
 x = df['chi_square_emcee']
 print("average chi square = ", sum(x)/len(x))
